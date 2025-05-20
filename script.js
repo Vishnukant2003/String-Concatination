55ad9b16eaad394cce4266fa6ab0c0117b78c726
Vishnukant Dnyanoba Gadekar <157220206+Vishnukant2003@users.noreply.github.com>
2025-05-20 16:40:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+let Name =" Vishnuklant "
+ let city = "pune" 
+
+ let sentence = "My name is " + Name + " and I live in " + city ;
+
+ //OR 
+
+ let sentence2 = `My name is ${Name} and I live in ${city}`
+
+ console.log(sentence2);
+ console.log(sentence);
+
+
+
+ // ternory operator
+
+ let age = 18;
+
+ let message = age >= 18 ? "You can vote" : "You can't vote";
